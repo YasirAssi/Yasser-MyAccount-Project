01e8f2f5af4363de5632421d06c938072c8d575c
YasirAssi <144779590+YasirAssi@users.noreply.github.com>
2023-11-26 14:49:09 +0200

    my account project

---
+++ myAccountscript.js
@@ -0,0 +1,9 @@
+//incase of several export
+// import {  } from "module";
+
+//incase of export default
+import Action from "./classes/action.js";
+let salary = new Action("income", "November Salary", 1000);
+console.log(salary);
+let supermarket= new Action("expense", "Supermarket", 600);
+console.log(supermarket);
