c544a1e03b3ced8fca48e3c520cd29314117bb27
YasirAssi <144779590+YasirAssi@users.noreply.github.com>
2023-11-28 14:37:48 +0200

    Update ActionManager.js

--- classes/ActionManager.js
+++ classes/ActionManager.js
@@ -1,31 +1,61 @@
-export default class ActionManager {
-    constructor(){
-        this.balance = 0;
-        this.actions = [];
+//incase of several export
+// import {  } from "module";
+
+//incase of export default
+import Action from "./classes/action.js";
+import ActionManager from "./classes/ActionManager.js";
+
+let salary = new Action("income", "November Salary", 10000);
+console.log(salary);
+let supermarket= new Action("expense", "Supermarket", 600);
+console.log(supermarket);
+
+let manager = new ActionManager();
+manager.addAction(salary);
+manager.addAction(supermarket);
+console.log(manager.actions);
+
+window.addActionToManager = () => { 
+//get data from form
+let type = document.getElementById('type').value;
+let description = document.getElementById('description').value;
+let amount = +document.getElementById('amount').value;
+//create the action object
+let action = new Action (type, description, amount);
+//to add the action to actionManager
+manager.addAction(action)
+showActionsInTable();
+//reset the form
+document.getElementById('description').value = '';
+document.getElementById('amount').value = '';
+}
+
+window.deleteActionFromManager = (actionId) => {
+    if(confirm('delete?')){
+        manager.deleteAction(actionId);
+        showActionsInTable();
     }
-    addAction(action){
-        this.actions.push(action);
-        this.calcBalance();
+};
+
+window.updatedActionFromManager = (actionId) => {
+    let newAmount = prompt("update here")
+    if( newAmount == null || newAmount == '') alert ('try again!')
+    else{
+        manager.updateAction(actionId, +newAmount);
+        showActionsInTable();
     }
-    deleteAction(actionId){
-        //find the relevent index 
-        let deleteIndex =  this.actions.findIndex((action) => action.id == actionId);
-        //delete with splice
-        this.actions.splice(deleteIndex, 1);
-        this.calcBalance();
-    }
-    updateAction(actionId,newAmount){
-        let updateAction = this.actions.findIndex((action) => action.id ==actionId);
-        this.actions[updateAction].amount = this.actions[updateAction].description == 'income' ? newAmount : -newAmount;
-        this.calcBalance();
-    }
-    calcBalance(){
-        this.balance = 0;
-        // to do also with reduce: // 
-        this.balance = this.actions.reduce((amount, action) => amount + action.amount, 0);
-        // for(let action of this.actions){
-        //     this.balance += action.amount;
-        // }
-        document.getElementById('balance').innerHTML = `Balance: ${this.balance}`;
+};
+
+function showActionsInTable() {
+    document.getElementById('actions').innerHTML = '';
+    for(let action of manager.actions){
+    document.getElementById('actions').innerHTML += 
+    `<tr class=${action.type == 'income' ? 'text-success' : 'text-danger'}>
+    <td>${action.description}</td>
+    <td>${action.amount}</td>
+    <td> <a onclick="updatedActionFromManager(${action.id})"> <i style="cursor: pointer" class="fa-regular fa-pen-to-square"></i></a></td>
+    <td> <a onclick="deleteActionFromManager(${action.id})"><i style="cursor: pointer" class="fa-solid fa-trash"></i></a></td>
+    </tr>`
     }
 }
+showActionsInTable();
